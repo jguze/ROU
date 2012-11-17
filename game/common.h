388b06b895c3dfa900a6557ee9c4e2fa57b78030
Justin Guze <jaguze@gmail.com>
2012-11-16 23:43:59 -0800

    Added our common.h to use as needed

---
+++ game/common.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
